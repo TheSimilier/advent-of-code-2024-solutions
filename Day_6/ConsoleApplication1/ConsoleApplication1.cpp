2e52ce0f14c83e162472c9c48a171862e61094ff
TheSimilier <simon.gemmel@gmail.com>
2024-12-06 17:38:23 +0100

    day6 bruteforce part 2

--- Day_6/ConsoleApplication1/ConsoleApplication1.cpp
+++ Day_6/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,30 +3,103 @@
 #include <string>
 #include <vector>
 #include <tuple>
+#include <unordered_map>
+#include <deque>
 
 struct Point {
     uint16_t x;
     uint16_t y;
+
+    bool operator==(Point p) const {
+        return (p.x == x && p.y == y);
+    }
 };
 
 struct Direction {
     int16_t x;
     int16_t y;
+
+    bool operator==(Direction d) const {
+        return (d.x == x && d.y == y);
+    }
 };
 
-struct IncompleteRectangle {
-    Point p1, p2, p3;
+template <>
+struct std::hash<Point> {
+    std::size_t operator()(Point const& s) const {
+        return ((std::hash<uint16_t>()(s.x)
+            ^ (std::hash<uint16_t>()(s.y) << 1)) >> 1);
+    }
 };
 
-struct PotentialRectangleCandidate {
-    Point p1;
-    Direction walkingDir;
+template<>
+struct std::hash<Direction> {
+    std::size_t operator()(Direction const& s) const {
+        return ((std::hash<uint16_t>()(s.x)
+            ^ (std::hash<uint16_t>()(s.y) << 1)) >> 1);
+    }
 };
+
+bool testBlockForLoop(const std::vector<std::vector<char>>& charMap, std::unordered_map<Point, Direction>& walkedRoute, std::deque<Point>& remainingBlocker,
+    const Point& startPosition, const Point& block) {
+	Direction currentDirection{};
+	currentDirection.x = 0;
+	currentDirection.y = -1;
+	auto currentPosition = startPosition;
+    bool trackingActive = false;
+    uint16_t counterSinceLastMove = 0;
+    std::unordered_map<Point, std::unordered_map<Direction, uint16_t>> pointToVisitCount;
+	while (currentPosition.x < charMap[0].size() && currentPosition.x >= 0 && currentPosition.y < charMap.size() && currentPosition.y >= 0) {
+            if (currentPosition.y + currentDirection.y >= charMap.size() || currentPosition.y + currentDirection.y < 0
+                || currentPosition.x + currentDirection.x >= charMap[0].size() || currentPosition.x + currentDirection.x < 0) break;
+		const Point nextPoint{
+			.x = static_cast<uint16_t>(currentPosition.x + currentDirection.x),
+			.y = static_cast<uint16_t>(currentPosition.y + currentDirection.y),
+		};
+		auto& nextChar = charMap[nextPoint.y][nextPoint.x];
+        const bool hitBlocker = nextPoint.x == block.x && nextPoint.y == block.y;
+
+		if (nextChar == '#' || hitBlocker) {
+			auto prevX = currentDirection.x;
+			currentDirection.x = -1*currentDirection.y;
+			currentDirection.y = prevX;
+            ++counterSinceLastMove;
+            if (counterSinceLastMove > 5) return false;
+			continue;
+		}
+
+		if (nextChar == '.') {
+            counterSinceLastMove = 0;
+			currentPosition.x += currentDirection.x;
+			currentPosition.y += currentDirection.y;
+		}
+
+        if (pointToVisitCount.find(currentPosition) != pointToVisitCount.end()) {
+            auto& map = pointToVisitCount[currentPosition];
+            auto it = map.find(currentDirection);
+            if (it != map.end()) {
+                map[currentDirection]++;
+                if (map[currentDirection] > 3) return true;
+            }
+            else {
+                map.emplace(currentDirection, 1);
+            }
+        }
+        else {
+            std::unordered_map<Direction, uint16_t> directionToCount;
+            directionToCount.emplace(currentDirection, 1);
+            pointToVisitCount.emplace(currentPosition, directionToCount);
+        }
+	}
+	return false;
+}
 
 int main()
 {
     std::vector<std::vector<char>> charMap;
-    Point currentPosition{};
+    Point startPosition{};
+    std::unordered_map<Point, Direction> walkedSquareToWalkedDirection;
+    std::deque<Point> stackOfRouteForBlocking;
 
 
 
@@ -40,8 +113,8 @@ int main()
         std::vector<char> row;
         for (char currChar : line) {
             if (currChar == '^') {
-                currentPosition.x = columnIndex;
-                currentPosition.y = lineIndex;
+                startPosition.x = columnIndex;
+                startPosition.y = lineIndex;
                 currChar = '.';
             }
             row.push_back(currChar);
@@ -53,110 +126,55 @@ int main()
     fileIn.close();
 
     uint16_t stepCount = 0;
-    std::vector<IncompleteRectangle> incompleteRectangles;
-    std::vector<PotentialRectangleCandidate> potentialCandidates;
+    auto charMap2 = charMap;
 
     {
-		IncompleteRectangle startingRectangle{};
-		startingRectangle.p1 = currentPosition;
-		incompleteRectangles.push_back(startingRectangle);
-
 		Direction currentDirection{};
 		currentDirection.x = 0;
 		currentDirection.y = -1;
-        bool turned = false;
-		while (currentPosition.x < charMap[0].size() - 1 && currentPosition.x > 0 && currentPosition.y < charMap.size() - 1 && currentPosition.y > 0) {
+        auto currentPosition = startPosition;
+		while (currentPosition.x < charMap[0].size() && currentPosition.x >= 0 && currentPosition.y < charMap.size() && currentPosition.y >= 0) {
+            if (currentPosition.y + currentDirection.y >= charMap.size() || currentPosition.y + currentDirection.y < 0
+                || currentPosition.x + currentDirection.x >= charMap[0].size() || currentPosition.x + currentDirection.x < 0) break;
 			auto& nextChar = charMap[currentPosition.y + currentDirection.y][currentPosition.x + currentDirection.x];
 			if (nextChar == '#') {
 				auto prevX = currentDirection.x;
 				currentDirection.x = -1*currentDirection.y;
 				currentDirection.y = prevX;
 
-				//add current point to the last 3 incomplete rectangles
-				for (int index = std::ssize(incompleteRectangles) - 1; index >= 0 && index >= std::ssize(incompleteRectangles) - 2; index--) {
-					auto& rect = incompleteRectangles[index];
-					if (rect.p2.x == 0) {
-						rect.p2 = currentPosition;
-						continue;
-					}
-					if (rect.p3.x == 0) {
-						rect.p3 = currentPosition;
-					}
-				}
-				IncompleteRectangle newRect{};
-				newRect.p1 = currentPosition;
-				incompleteRectangles.push_back(newRect);
-                turned = true;
 				continue;
 			}
-
-            auto leftChar = charMap[currentPosition.y + -1 * currentDirection.x][currentPosition.x + currentDirection.y];
-            if (!turned && leftChar == '#') {
-                potentialCandidates.push_back({
-                    .p1 = currentPosition,
-                    .walkingDir = currentDirection,
-                    });
-            }
 
 			if (nextChar == '.' || nextChar == 'X') {
 				if (nextChar != 'X') ++stepCount;
 				nextChar = 'X';
 				currentPosition.x += currentDirection.x;
 				currentPosition.y += currentDirection.y;
-                turned = false;
+
+                walkedSquareToWalkedDirection.emplace(currentPosition, currentDirection);
+                if (currentPosition != startPosition)stackOfRouteForBlocking.push_back(currentPosition);
 			}
 		}
     }
 
-    //iterate over incomplete rectangle list
-    //see if 3 points create a valid axis-aligned rectangle
-    //find 4th point
-    //traverse along the 4 points in order with distance of circumference of the rectangle
-    //if traversal leads back to 4th point a loop point is found
-    //verify loop point doesnt break prior traversal?
     uint16_t loopCount = 0;
-    for (const auto& rect : incompleteRectangles) {
-        if (rect.p1.x == 0 || rect.p2.x == 0 || rect.p3.x == 0) continue;
-        const auto& p1 = rect.p1;
-        const auto& p2 = rect.p2;
-        const auto& p3 = rect.p3;
-
-        const Point p4{
-            .x = static_cast<uint16_t>(p3.x - (p2.x - p1.x)),
-            .y = static_cast<uint16_t>(p3.y - (p2.y - p1.y)),
-        };
-
-        Direction currentDirection{};
-        const auto deltaX = p1.x - p4.x;
-        const auto deltaY = p1.y - p4.y;
-        currentDirection.x = deltaX > 0 ? 1 : deltaX < 0 ? -1 : 0;
-        currentDirection.y = deltaY > 0 ? 1 : deltaY < 0 ? -1 : 0;
-
-        const auto walkDistance = 2 * (std::abs(p1.x - p2.x) + std::abs(p1.y - p2.y)) + 2 * (std::abs(p2.x - p3.x) + std::abs(p2.y - p3.y));
-        auto testPosition = p4;
-
-        for (int i = walkDistance; i > 0; i--) {
-			auto& nextChar = charMap[testPosition.y + currentDirection.y][testPosition.x + currentDirection.x];
-
-            if (nextChar == '#') {
-                auto prevX = currentDirection.x;
-                currentDirection.x = -1 * currentDirection.y;
-                currentDirection.y = prevX;
-                continue;
+    std::unordered_map<Point, bool> pointToHitLoop;
+    for (uint16_t x = std::size(charMap2[0]); x >= 1; x--) {
+        for (uint16_t y = std::size(charMap); y >= 1; y--) {
+            Point currBlocker{ .x = static_cast<uint16_t>(x-1), .y = static_cast<uint16_t>(y-1) };
+            if (currBlocker == startPosition) continue;
+            if (pointToHitLoop.contains(currBlocker)) continue;
+            bool hitLoop = false;
+            if (testBlockForLoop(charMap2, walkedSquareToWalkedDirection, stackOfRouteForBlocking, startPosition, currBlocker)) {
+                hitLoop = true;
+                ++loopCount;
             }
-
-			if (nextChar == '.' || nextChar == 'X') {
-				currentPosition.x += currentDirection.x;
-				currentPosition.y += currentDirection.y;
-			}
+            pointToHitLoop.emplace(currBlocker, hitLoop);
         }
-
-        if (testPosition.x != p4.x && testPosition.y != p4.y) continue;
-
-        ++loopCount;
+        std::printf("column %i done\n", x);
     }
 
     std::printf("loop count: %i\n", loopCount);
-    std::printf("step count: %i\n", stepCount +1);
+    std::printf("step count: %i\n", stepCount);
 }
 
