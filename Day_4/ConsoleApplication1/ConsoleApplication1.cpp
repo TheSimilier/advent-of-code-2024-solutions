495187bdf013046fc4d682dc6367357fcb4502c3
TheSimilier <simon.gemmel@gmail.com>
2024-12-06 07:36:48 +0100

    cleanup day4

--- Day_4/ConsoleApplication1/ConsoleApplication1.cpp
+++ Day_4/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,3 @@
-// ConsoleApplication1.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -77,17 +74,7 @@ int main()
         charMap.push_back(row);
         ++lineIndex;
     }
+	fileIn.close();
     puzzle1(charMap, centerXPositions);
     puzzle2(charMap, centerAPositions);
 }
-
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
